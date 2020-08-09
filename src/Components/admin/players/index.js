39aaf937a06c5da8a8f57cbf0bd19f255f323cd9
boka88 <boka8boka@gmail.com>
2020-08-09 11:16:44 +0200

    Update Players sectin in Dashboard

--- src/Components/admin/players/index.js
+++ src/Components/admin/players/index.js
@@ -23,14 +23,72 @@ class AdminPlayers extends Component {
     }
 
     componentDidMount(){
-        firebasePlayers.once('value').then(())
+        firebasePlayers.once('value').then((snapshot)=>{
+            const players = firebaseLooper(snapshot)
+
+            this.setState({
+                isloading: false,
+                players: reverseArray(players)
+            })
+        })
     }
 
     render() {
         return (
-            <div>
-                players
-            </div>
+            <AdminLayout>
+                <div>
+                    <Paper>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>First name</TableCell>
+                                    <TableCell>Last name</TableCell>
+                                    <TableCell>Number</TableCell>
+                                    <TableCell>Position</TableCell>
+                               </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                { this.state.players ?
+                                    this.state.players.map((player,i)=>(
+                                        <TableRow key={i}>
+                                            <TableCell>
+                                               <Link to={`/admin_players/add+players/${player.id}`}>
+                                                   {player.name}
+                                               </Link>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Link to={`/admin_players/add+players/${player.id}`}>
+                                                   {player.lastname}
+                                               </Link>
+                                            </TableCell>
+                                            <TableCell>
+                                                   {player.number}
+                                            </TableCell>
+                                            <TableCell>
+                                                   {player.position}
+
+                                            </TableCell>
+                                       </TableRow>
+                                    ))
+
+                                    :null
+
+                                }
+                            </TableBody>
+                        </Table>
+
+                    </Paper>
+                <div className='admin_progress'>
+                    {this.state.isloading ?
+                            <CircularProgress thickness={7} style={{color: '#98c5e9'}}/>
+                        :''
+
+                    }
+                </div>
+
+                </div>
+
+            </AdminLayout>
         )
     }
 }
